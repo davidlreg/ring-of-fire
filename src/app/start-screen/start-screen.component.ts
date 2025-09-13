b6ecfcdf8a4fb81d59fcb54a3df4b31484583ba1
David Gerliz <davidgerliz@MacBook-Air-von-David.local>
2025-09-13 10:25:57 +0200

    added cards and blank start screen compopnent

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-start-screen',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+}
