575bc15927972a2212333ecbd08914eb0abb57cb
David Gerliz <davidgerliz02@gmail.com>
2025-10-15 22:34:02 +0200

    added animation to uncover card

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  newGame() {
+    // Start Game
+    this.router.navigateByUrl('/game');
+  }
 }
